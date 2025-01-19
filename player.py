985c660cb90228ae212da30a5a256f1e7f01fcb8
DavidinhoHD <davidinho.minecraft@gmail.com>
2025-01-19 21:51:13 +0100

    add shots

--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
 import pygame
+
 
 class Player(CircleShape):
     def __init__(self, x, y):
@@ -41,8 +43,15 @@ class Player(CircleShape):
 
         if keys[pygame.K_s]:
             self.move(-dt)
+        
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        bullet.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
-        self.position += forward * PLAYER_SPEED * dt
+        self.position += forward * PLAYER_SHOOT_SPEED * dt
---
+++ shot.py
@@ -0,0 +1,15 @@
+import pygame
+from circleshape import CircleShape
+from constants import *
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+        pygame.sprite.Sprite.__init__(self, self.containers)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white",[self.position.x, self.position.y], SHOT_RADIUS, 2)
+        
+    def update(self, dt):
+        self.position += self.velocity * dt 
